b1003cd1096e6ceddb298967c2d2e04d48355382
DESKTOP-T7PH5A9\nada <nadahassanin189@gmail.com>
2026-04-10 23:36:07 +0200

    Q2

--- Program.cs
+++ Program.cs
@@ -11,6 +11,21 @@
             // d) Aggregation:  If Team is deleted, Players stay alive.
             // e) Dependency:  The method only needs the Logger for a moment.
             #endregion
+            #region Q2: Modifiers & Sealed
+            // a) Protected: Yes, a child can see it in another assembly. 
+            //    Outside instance? No, only children can see it.
+
+            // b) Differences:
+            //    Protected Internal: Child OR anyone in the same assembly.
+            //    Private Protected: Child ONLY if it's in the same assembly.
+
+            // c) Sealed:
+            //    On Class: No one can inherit from it (The end of the family tree).
+            //    On Method: No child can override it (Change its behavior).
+
+            // d) New Sealed Class: Yes! You can use 'new' normally. 
+            //    Sealed only stops inheritance, not creating objects.
+            #endregion
         }
     }
 }
