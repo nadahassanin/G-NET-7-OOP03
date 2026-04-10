ccacd85344cd292484f262b99264113d5641aec5
DESKTOP-T7PH5A9\nada <nadahassanin189@gmail.com>
2026-04-10 23:27:36 +0200

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1: Relationships
+            // a) Composition:  If University dies, Departments die too.
+            // b) Association:  Driver and Car just use each other.
+            // c) Inheritance:  A Dog is a type of Animal.
+            // d) Aggregation:  If Team is deleted, Players stay alive.
+            // e) Dependency:  The method only needs the Logger for a moment.
+            #endregion
         }
     }
 }
